47a59bed0b31095032987ab60ced42d620f95f38
Kriti-indixpert <kritikumari86033909@gmail.com>
2026-07-27 03:13:38 +0530

    Improve homepage responsive design

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,6 +4,46 @@ import '../scss/style.scss'
 // Import all of Bootstrap’s JS
 import * as bootstrap from 'bootstrap'
 
+// trending
 
+
+
+import Swiper from "swiper";
+import { Autoplay, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+new Swiper(".reviewSwiper", {
+
+    modules: [Autoplay, Pagination],
+
+    slidesPerView: 1,
+
+    loop: true,
+
+    speed: 500,
+
+    grabCursor: true,
+
+    autoplay: {
+
+        delay: 1200,
+
+        disableOnInteraction: false,
+
+        pauseOnMouseEnter: true,
+
+    },
+
+    pagination: {
+
+        el: ".swiper-pagination",
+
+        clickable: true,
+
+    },
+
+});
 
 
